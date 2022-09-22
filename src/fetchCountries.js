f0dea7d0ea4e07fe2e5602fb06291ae37d2d7907
Bohdan <makohoniuk.bo@gmail.com>
2022-09-22 20:51:09 +0300

    змінює функцію запиту

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,6 @@
 export function fetchCountries(name){
    return fetch (
-   `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+   `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
    ).then(r => {
     if (!r.ok){
         throw new Error (r.status);
@@ -8,3 +8,4 @@ export function fetchCountries(name){
     return response.json();
    });
 }
+const BASE_URL = 'https://restcountries.com/v3.1';
